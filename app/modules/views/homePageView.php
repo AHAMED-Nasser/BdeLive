98b64afc9717d46b0bdeca3e3d13089808750577
romain <romain.cantor13@gmail?com>
2025-10-14 21:14:15 +0200

    Modif caroussel, ajout section "BDE", et bouton remontant

--- app/modules/views/homePageView.php
+++ app/modules/views/homePageView.php
@@ -1,8 +1,6 @@
 <?php
     start_page("BDE Inform'Aix - Site Officiel", true);
 ?>
-    <main>
-
         <?php
         // Start session if not already started
         if (session_status() === PHP_SESSION_NONE) {
@@ -29,6 +27,7 @@
         </section>
 
         <section class="events" aria-labelledby="events-title">
+            <h3 id="big-title"><strong>Bienvenue au BDE Informatique d’Aix !</strong></h3>
             <h2 id="events-title">Événements à venir</h2>
             <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
                 <ul class="carousel-indicators">
@@ -58,5 +57,25 @@
             </div>
         </section>
 
-    </main>
+
+        <section class="BDE" aria-labelledby="bde-title">
+            <h3> <strong>Qui sommes nous ?</strong></h3>
+            <div class="bde-text">
+                <p>
+                    <h2>Le <strong>BDE Informatique d’Aix</strong> est une association étudiante qui rassemble les passionnés de
+                    technologies, de programmation et de cybersécurité.
+                    Nous organisons des événements, des ateliers et des soirées pour créer du lien entre les étudiants
+                    en informatique et favoriser l’entraide au sein du campus.
+
+                    Nos valeurs : <strong>convivialité, partage et innovation</strong>.
+                    Que tu sois développeur, gamer ou simplement curieux, le BDE Info Aix est fait pour toi !
+                </p> </h2>
+            </div>
+        </section>
+            <a href="index.php" class="btn-image-right">
+                <img src="./assets/img/fleche-accueil.png" alt="Aller à l'accueil" class="bde-image">
+            </a>
+
+
+</main>
 <?php end_page() ?>
