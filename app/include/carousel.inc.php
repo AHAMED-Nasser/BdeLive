f1d8d87950099c5bded96cd62a0549dc9bd34c6a
nularyNasser <137123083+nularyNasser@users.noreply.github.com>
2025-10-16 13:47:05 +0200

    Ajustement du fichier

--- app/include/carousel.inc.php
+++ app/include/carousel.inc.php
@@ -1,26 +1,28 @@
 <?php
-    function useCarousel($carouselLabel, $imageMap) {
+    function useCarousel($carouselLabel, $imageMap, $carouselId) {
 ?>
 
 <h2 class="event-title"><?= $carouselLabel ?></h2>
-<section class="carousel" id="<?= $carouselId ?? 'carousel' ?>">
-    <button class="carousel-control prev" onclick="moveSlide(-1, '<?= $carouselId ?? 'carousel' ?>')" ><img src="./assets/img/carousel/arrow.png" width="50px"></button>
+<article class="carousel" id="<?= $carouselId ?? 'carousel' ?>">
+    <div class="carousel-block">
+        <button class="carousel-control prev" onclick="moveSlide(-1, '<?= $carouselId ?? 'carousel' ?>')" ><img src="./assets/img/carousel/arrow.png" alt="Précédent"></button>
 
-    <div class="carousel-inner">
-        <?php foreach ($imageMap as $index => $image): ?>
-        <div class="carousel-item <?= $index === 0 ? 'active' : '' ?>">
-            <img src="<?= htmlspecialchars($image['src']) ?>" class="carousel-image" width="800px">
+        <div class="carousel-inner">
+            <?php foreach ($imageMap as $index => $image): ?>
+                <div class="carousel-item <?= $index === 0 ? 'active' : '' ?>">
+                    <img src="<?= htmlspecialchars($image['src']) ?>" class="carousel-image" alt="<?= htmlspecialchars($image['alt'] ?? 'Image') ?>">
+                </div>
+            <?php endforeach ?>
         </div>
-        <?php endforeach ?>
-    </div>
 
-    <button class="carousel-control next" onclick="moveSlide(1, '<?= $carouselId ?? 'carousel'?>')"><img src="./assets/img/carousel/arrow.png" width="50px"></button>
+        <button class="carousel-control next" onclick="moveSlide(1, '<?= $carouselId ?? 'carousel'?>')"><img src="./assets/img/carousel/arrow.png" alt="Suivant"></button>
+    </div>
 
     <div class="carousel-dots">
-        <?php foreach ($imageMap as $image): ?>
-            <span class="dot <?= $index === 0 ? 'active' : ''?>" onclick="currentSlide(<?= $index ?>, <?= $carouselId ?? 'carousel'?>)"></span>
+        <?php foreach ($imageMap as $index => $image): ?>
+            <span class="dot <?= $index === 0 ? 'active' : ''?>" onclick="currentSlide(<?= $index ?>, '<?= $carouselId ?? 'carousel'?>')"></span>
         <?php endforeach ?>
     </div>
-</section>
+</article>
 
 <?php } ?>
