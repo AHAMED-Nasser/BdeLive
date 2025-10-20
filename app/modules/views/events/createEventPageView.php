2864c875aa844c6944d27141474c83988faa3d74
nularyNasser <nahamed.sveil@gmail.com>
2025-10-20 17:18:12 +0200

    Création de la page de création d'événément

--- app/modules/views/events/createEventPageView.php
+++ app/modules/views/events/createEventPageView.php
@@ -1,7 +1,41 @@
 <?php start_page("Créer un événement") ?>
 
 <section class="createEvent">
+    <div class="forgot-container">
+        <h1 class="title">Création d'un événement</h1>
 
+        <form id="form" action="index.php?page=event" method="POST">
+            <label for="event-name">Nom de l'événement</label>
+            <input id="event-name" type="text" name="event-name" placeholder="Nom de l'événement" required>
+
+            <label for="event-date">Date de l'événement</label>
+            <input id="event-date" type="date" name="event-date" required>
+
+            <label for="event-time">Heure de l'événement</label>
+            <input id="event-time" type="time" name="event-time">
+
+            <label for="event-place">Lieu de l'événement</label>
+            <input id="event-place" type="text" placeholder="Entrer votre lieu">
+
+            <label for="event-theme">Thème de l'événement</label>
+            <input id="event-theme" type="text" placeholder="Entrer le thème de l'événement (Soirée, ...)">
+
+
+        </form>
+
+    </div>
+    <script>
+        const date = new Date();
+        // Date
+        const today = date.toISOString().split("T")[0];
+        document.getElementById('event-date').setAttribute("value", today)
+
+        // Time
+        const hours = String(date.getHours()).padStart(2, '0');
+        const minutes = String(date.getMinutes()).padStart(2, '0');
+        document.getElementById('event-time').value = `${hours}:${minutes}`;
+    </script>
 </section>
+
 
 <?php end_page() ?>
