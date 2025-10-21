f806027a4f0eacdf5d08635b1cc5d336da5a3bbb
chetiouiwillem <willem.chetioui@etu.univ-amu.fr>
2025-10-21 09:19:54 +0200

    fix: Modification du chemin pour LoadView

--- app/modules/controllers/ProfileController.php
+++ app/modules/controllers/ProfileController.php
@@ -6,6 +6,6 @@ class ProfileController
     }
 
     public function loadView(string $viewName): void {
-        require_once __DIR__ . '/../views/' . $viewName . '.php';
+        require_once __DIR__ . '/../views/users/' . $viewName . '.php';
     }
 }
