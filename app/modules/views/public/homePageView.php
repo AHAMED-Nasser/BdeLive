4d44c42dea0537f8eccf411dbf7b31e192bbf4e6
romain <romain.cantor13@gmail?com>
2025-10-22 11:39:29 +0200

    push dans DEV(deleteAccount)

--- app/modules/views/public/homePageView.php
+++ app/modules/views/public/homePageView.php
@@ -10,7 +10,7 @@
     <main>
 
         <?php
-        // Start session if not already started/
+        // Startsession if not already started/
         if (session_status() === PHP_SESSION_NONE) {
             session_start();
         }
