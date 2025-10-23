4fd6f0a06c68b4715b87d908b992952e004210be
chetiouiwillem <willem.chetioui@etu.univ-amu.fr>
2025-10-23 13:43:57 +0200

    fix : Mise en place de la structure de la page

--- app/modules/views/users/profilePageView.php
+++ app/modules/views/users/profilePageView.php
@@ -2,9 +2,24 @@
     start_page("Modification du profil", True);
 ?>
 
-<div class="change-username">
+<div class="change-user">
+    <h1>Modification des informations</h1>
     <form action="index.php?page=profile" method="post">
-
+        <div class="change-user-div">
+            <label class="change-user-label">Prénom</label>
+            <input class="change-user-input" placeholder="Changement du prénom">
+            <button class="change-user-button">Confirmer le changement de prénom</button>
+        </div>
+        <div class="change-user-div">
+            <label class="change-user-label">Nom</label>
+            <input class="change-user-input" placeholder="Changement du nom">
+            <button class="change-user-button">Confirmer le changement du nom</button>
+        </div>
+        <div class="change-user-div">
+            <label class="change-user-label">Mail</label>
+            <input class="change-user-input" placeholder="Nouvelle adresse mail">
+            <button class="change-user-button">Confirmer la nouvelle adresse mail</button>
+        </div>
     </form>
 </div>
 
