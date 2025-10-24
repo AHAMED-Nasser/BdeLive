c6ea1d65cd437ff4cf062c0d1e386297189b70f5
chetiouiwillem <willem.chetioui@etu.univ-amu.fr>
2025-10-24 10:39:28 +0200

    feat : Ajout du changement de mot de passe et ajout des informations actuelles

--- app/modules/views/users/profilePageView.php
+++ app/modules/views/users/profilePageView.php
@@ -3,23 +3,49 @@
 ?>
 
 <div class="change-user">
+
     <h1>Modification des informations</h1>
-    <form action="index.php?page=profile" method="post">
+
+    <form method="POST" action="index.php?page=profile">
+
         <div class="change-user-div">
-            <label class="change-user-label">Prénom</label>
-            <input class="change-user-input" placeholder="Changement du prénom">
-            <button class="change-user-button">Confirmer le changement de prénom</button>
+            <div>
+                <label class="change-user-label">Prénom</label>
+                <?php if (isset($_SESSION['first_name'])) : ?>
+                <span>(Actuel : <?= htmlspecialchars($_SESSION['first_name'])?>)</span>
+                <?php endif ?>
+            </div>
+            <input class="change-user-input" placeholder="Saisissez le nouveau prénom" name="first-name">
+            <button class="change-user-button" type="submit" name="ok_first_name">Confirmer</button>
+        </div>
+
+        <div class="change-user-div">
+            <div>
+                <label class="change-user-label">Nom</label>
+                <?php if (isset($_SESSION['last_name'])) : ?>
+                    <span>(Actuel : <?= htmlspecialchars($_SESSION['last_name'])?>)</span>
+                <?php endif?>
+            </div>
+            <input class="change-user-input" placeholder="Saisissez le nouveau nom" name="last-name">
+            <button class="change-user-button">Confirmer</button>
+        </div>
+
+        <div class="change-user-div">
+            <div>
+                <label class="change-user-label">Mail</label>
+                <?php if (isset($_SESSION['email'])) : ?>
+                    <span>(Actuel : <?= htmlspecialchars($_SESSION['email'])?>)</span>
+                <?php endif?>
+            </div>
+            <input class="change-user-input" placeholder="Entrez votre nouvelle adresse mail">
+            <button class="change-user-button">Confirmer</button>
         </div>
         <div class="change-user-div">
-            <label class="change-user-label">Nom</label>
-            <input class="change-user-input" placeholder="Changement du nom">
-            <button class="change-user-button">Confirmer le changement du nom</button>
-        </div>
-        <div class="change-user-div">
-            <label class="change-user-label">Mail</label>
-            <input class="change-user-input" placeholder="Nouvelle adresse mail">
-            <button class="change-user-button">Confirmer la nouvelle adresse mail</button>
+            <label class="change-user-label">Mot de passe</label>
+            <input class="change-user-input" placeholder="Entrez votre mot de passe actuel" type="password">
+            <input class="change-user-input" placeholder="Entrez votre nouveau mot de passe" type="password">
+            <input class="change-user-input" placeholder="Confirmez votre nouveau mot de passe" type="password">
+            <button class="change-user-button">Confirmer</button>
         </div>
     </form>
 </div>
-
