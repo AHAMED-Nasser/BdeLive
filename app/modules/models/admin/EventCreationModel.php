e7bda1222f907b2b62163c47de8fbe40b7c2148c
Amin HELALI <amin.helali@etu.univ-amu.fr>
2025-10-24 16:00:57 +0200

    Fix:suppression des controller dupliquer, function non utiliser.

--- app/modules/models/admin/EventCreationModel.php
+++ app/modules/models/admin/EventCreationModel.php
@@ -7,19 +7,6 @@ class EventCreationModel {
         $this->pdo = Database::getInstance()->getConnection();
     }
 
-    public function isAdmin(int $userId): bool {
-        try {
-            $query = "SELECT role FROM users WHERE role='admin' AND user_id = :user_id";
-            $stmt = $this->pdo->prepare($query);
-            $stmt->bindParam(':user_id', $userId, PDO::PARAM_INT);
-            $stmt->execute();
-            $result = $stmt->fetch();
-            return $result !== false;
-        } catch (PDOException $e) {
-            error_log('EventCreationModel::isAdmin - ' . $e->getMessage());
-            return false;
-        }
-    }
 
     public function insertEvent(string $eventName,
                                 DateTime $eventDate,
