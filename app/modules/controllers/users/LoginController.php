dadb55f87135d4774ea2af86b36b73b496cf31ec
Thomas Palot <thomas.palot@etu.univ-amu.fr>
2025-11-03 09:14:55 +0100

    corrections phpstan niveau 8

--- app/modules/controllers/users/LoginController.php
+++ app/modules/controllers/users/LoginController.php
@@ -107,7 +107,7 @@ class LoginController
         // Attempt login
         if ($this->authController->login($email, $mdp)) {
             // Login successful
-            $_SESSION['success'] = 'Connexion réussie ! Bienvenue ' . htmlspecialchars($this->authController->getCurrentUserFullName()) . ' !';
+            $_SESSION['success'] = 'Connexion réussie ! Bienvenue ' . htmlspecialchars($this->authController->getCurrentUserFullName() ?? '') . ' !';
             header('Location: index.php?page=home');
             exit;
         } else {
