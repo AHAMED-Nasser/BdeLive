4aaf9d85b0bf0429ee4be7497fc249897b06d56c
chetiouiwillem <willem.chetioui@etu.univ-amu.fr>
2025-11-04 14:19:09 +0100

    fix : ajout des fonctions du controller non ajoutées au cours du merge

--- app/modules/controllers/users/ProfileController.php
+++ app/modules/controllers/users/ProfileController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Modules\Controllers\Users;
 
 use App\Modules\Controllers\AuthenticatedController;
+use App\Modules\Models\Users\UserManager;
 
 class ProfileController extends AuthenticatedController
 {
@@ -12,6 +13,52 @@ class ProfileController extends AuthenticatedController
     {
         parent::__construct();
         $this->render('users/profilePageView');
+        $action = $_GET['action'] ?? '';
+        if ($action === 'processFirstName' && $_SERVER['REQUEST_METHOD'] === 'POST') {
+            $this -> processFirstName();
+        } else if ($action === 'processLastName' && $_SERVER['REQUEST_METHOD'] === 'POST') {
+            $this -> processLastName();
+        }
+    }
+
+    public function processFirstName() : void {
+        $userId = $_SESSION['user_id'];
+        $newFirstName = $_POST['first-name'] ?? '';
+
+        // Verify if first_name field is empty
+        if (empty($newFirstName)) {
+            $_SESSION['error'] = 'Veuillez remplir le champ first_name';
+            header("Location: index.php?page=profile");
+            exit;
+        }
+
+        $userModel = new UserManager();
+        $userModel->updateFirstName($userId, $newFirstName);
+        $_SESSION['first_name'] = $_POST['first-name'] ?? '';
+
+
+        header("Location: index.php?page=profile");
+        exit;
+    }
+
+    public function processLastName() : void {
+        $userId = $_SESSION['user_id'];
+        $newLastName = $_POST['last-name'] ?? '';
+
+        // Verify if first_name field is empty
+        if (empty($newLastName)) {
+            $_SESSION['error'] = 'Veuillez remplir le champ first_name';
+            header("Location: index.php?page=profile");
+            exit;
+        }
+
+        $userModel = new UserManager();
+        $userModel->updateLastName($userId, $newLastName);
+        $_SESSION['last_name'] = $_POST['last-name'] ?? '';
+
+
+        header("Location: index.php?page=profile");
+        exit;
     }
 
     protected function loadView(string $viewName): void
