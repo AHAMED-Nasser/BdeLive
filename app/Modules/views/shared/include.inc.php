40a6d91158cc04e9724b6da402fce905dda4ab90
nularyNasser <137123083+nularyNasser@users.noreply.github.com>
2026-01-07 22:36:47 +0100

    fix : ajout du css du calendrier

--- app/Modules/views/shared/include.inc.php
+++ app/Modules/views/shared/include.inc.php
@@ -88,6 +88,7 @@ function start_page(string $title, bool $wouldNav = true, ?array $user = null): 
     <link rel="stylesheet" href="./assets/css/articles.css">
     <link rel="stylesheet" href="./assets/css/homepage-articles.css">
     <link rel="stylesheet" href="./assets/css/admin.css">
+    <link rel="stylesheet" href="./assets/css/schedule.css">
     <title><?= $title ?></title>
 </head>
 <body>
