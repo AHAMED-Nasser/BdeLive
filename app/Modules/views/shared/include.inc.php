274f0d9bcd34df438f7dd4b11c41c747c1b63d49
nularyNasser <137123083+nularyNasser@users.noreply.github.com>
2026-01-07 22:03:52 +0100

    new : import du fichier css administration

--- app/Modules/views/shared/include.inc.php
+++ app/Modules/views/shared/include.inc.php
@@ -87,6 +87,7 @@ function start_page(string $title, bool $wouldNav = true, ?array $user = null): 
     <link rel="stylesheet" href="./assets/css/profile.css">
     <link rel="stylesheet" href="./assets/css/articles.css">
     <link rel="stylesheet" href="./assets/css/homepage-articles.css">
+    <link rel="stylesheet" href="./assets/css/admin.css">
     <title><?= $title ?></title>
 </head>
 <body>
