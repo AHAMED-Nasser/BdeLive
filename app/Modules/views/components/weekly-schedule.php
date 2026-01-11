1abb3e829e366e39a9b7c0290adac32ea136b196
nularyNasser <137123083+nularyNasser@users.noreply.github.com>
2026-01-11 17:22:29 +0100

    fix : bon chemin des fichier css

--- app/Modules/views/components/weekly-schedule.php
+++ app/Modules/views/components/weekly-schedule.php
@@ -57,9 +57,9 @@ if (!empty($schedule['weekDates'])) {
 }
 ?>
 
-<link rel="stylesheet" href="/assets/css/weekly-schedule.css">
-<link rel="stylesheet" href="/assets/css/calendar.css">
-<script src="/assets/js/schedule-modal.js" defer></script>
+<link rel="stylesheet" href="./assets/css/weekly-schedule.css">
+<link rel="stylesheet" href="./assets/css/calendar.css">
+<script src="./assets/js/schedule-modal.js" defer></script>
 
 <div class="weekly-schedule" role="region" aria-label="Emploi du temps semaine <?= $schedule['week'] ?>">
     
