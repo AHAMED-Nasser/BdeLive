405c6db94e6907aff82ee5028d284d6b1e9591e4
HELALI <amin.helali@etu.univ-amu.fr>
2026-01-11 15:17:50 +0100

    fix: simplification condition menu déroulant (PHPStan)

--- app/Modules/views/shared/include.inc.php
+++ app/Modules/views/shared/include.inc.php
@@ -141,14 +141,10 @@ function start_page(string $title, bool $wouldNav = true, ?array $user = null): 
                         <div class="profile-dropdown-menu">
                             <a href="index.php?page=profile"><i class="fas fa-id-card"></i> Mon Profil</a>
                             <a href="index.php?page=privacy"><i class="fas fa-shield-alt"></i> Confidentialité</a>
-                            <?php if ($isAdmin) : ?>
-                                <a href="index.php?page=createEvent"><i class="fas fa-plus-circle"></i> Créer un événement</a>
-                                <a href="index.php?page=createArticle"><i class="fas fa-edit"></i> Créer un article</a>
-                                <div class="dropdown-divider"></div>
-                                <a href="index.php?page=adminSection"><i class="fas fa-cogs"></i> Administration</a>
-                            <?php else : ?>
-                                <div class="dropdown-divider"></div>
-                            <?php endif; ?>
+                            <a href="index.php?page=createEvent"><i class="fas fa-plus-circle"></i> Créer un événement</a>
+                            <a href="index.php?page=createArticle"><i class="fas fa-edit"></i> Créer un article</a>
+                            <div class="dropdown-divider"></div>
+                            <a href="index.php?page=adminSection"><i class="fas fa-cogs"></i> Administration</a>
                             <a href="index.php?page=logout"><i class="fas fa-sign-out-alt"></i> Déconnexion</a>
                             <a href="index.php?page=deleteAccount" class="dropdown-danger"><i class="fas fa-trash-alt"></i> Supprimer mon compte</a>
                         </div>
