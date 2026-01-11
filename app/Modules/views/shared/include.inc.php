3f7974277e424dd8e8f43aa67a4943e226e7aaa0
nularyNasser <137123083+nularyNasser@users.noreply.github.com>
2026-01-11 16:52:49 +0100

    fix : import du css de eventView

--- app/Modules/views/shared/include.inc.php
+++ app/Modules/views/shared/include.inc.php
@@ -89,6 +89,7 @@ function start_page(string $title, bool $wouldNav = true, ?array $user = null): 
     <link rel="stylesheet" href="./assets/css/homepage-articles.css">
     <link rel="stylesheet" href="./assets/css/admin.css">
     <link rel="stylesheet" href="./assets/css/schedule.css">
+    <link rel="stylesheet" href="./assets/css/event.css">
     <title><?= $title ?></title>
 </head>
 <body>
