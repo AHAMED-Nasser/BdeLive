43c199fe85acab9f85522d8dd51535570acae0ec
nularyNasser <137123083+nularyNasser@users.noreply.github.com>
2026-01-16 16:49:23 +0100

    new : ajout du status d'ouverture du BDE

--- app/Modules/views/public/homePageView.php
+++ app/Modules/views/public/homePageView.php
@@ -55,6 +55,23 @@ if (isset($user) && !empty($user['delete_session_after_home'])) {
             <h1 id="hero-title">BDELive</h1>
             <p>Site officiel du BDE, BUT Informatique Aix-en-Provence</p>
         </div>
+
+        <div class="opening-hour">
+            <?php
+            $day = date('N');
+            $hour = date('H:i');
+            if (
+                $day >= 1 && $day <= 5 &&
+                ($hour >= '10:05' && $hour <= '10:25') ||
+                ($hour >= '12:15' && $hour <= '13:30') ||
+                ($hour >= '15:20' && $hour <= '15:40')
+            ) :
+                ?>
+            <span style="color: white; border-radius: 5px; padding: 13px; background-color: #25782d; letter-spacing: 1px">BDE Ouvert</span>
+            <?php else : ?>
+            <span style="color: white; border-radius: 5px; padding: 13px; background-color: #b23232; letter-spacing: 1px">BDE Fermé</span>
+            <?php endif ?>
+        </div>
         
         <!-- Horaires d'ouverture -->
         <div class="hero-hours">
