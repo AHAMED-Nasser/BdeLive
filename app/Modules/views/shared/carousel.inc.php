b2819342b691600e1e7ae67e8f240623a88f714b
AHAMED-Nasser <137123083+AHAMED-Nasser@users.noreply.github.com>
2026-03-09 14:18:58 +0100

    feat : ajout d'un badge indiquant si un événement est en cours, terminé ou à venir. (#261)
    Co-authored-by: nularyNasser <nahamed.sveil@gmail.com>

--- app/Modules/views/shared/carousel.inc.php
+++ app/Modules/views/shared/carousel.inc.php
@@ -13,7 +13,7 @@
  * @param array<int, array{src: string, alt?: string}> $imageMap
  * @param string $carouselId
  */
-function useCarousel($carouselLabel, $imageMap, $carouselId): void
+function useCarousel($carouselLabel, $imageMap, $carouselId, $event): void
 {
     ?>
 
@@ -29,13 +29,33 @@ function useCarousel($carouselLabel, $imageMap, $carouselId): void
 
             <div class="carousel-inner">
                 <?php foreach ($imageMap as $index => $image) : ?>
+<!--                    <div class="carousel-item --><?php //= $index === 0 ? 'active' : '' ?><!--">-->
+<!--                        --><?php //if ($event->getDate() < date('Y-m-d')) : ?>
+<!--                            <span style="position: absolute; left: 0; margin: 3px; padding: 5px 5px; background-color: red; border-radius: 50px">en cours</span>-->
+<!--                        --><?php //else : ?>
+<!--                            <span style="position: absolute; left: 0; margin: 3px; padding: 5px 5px; background-color: green; border-radius: 50px" >terminé</span>-->
+<!--                        --><?php //endif; ?>
+<!--                        <img src="--><?php //= htmlspecialchars($image['src']) ?><!--" class="carousel-image"-->
+<!--                            alt="--><?php //= htmlspecialchars($image['alt'] ?? ($carouselLabel . ' - Image ' . ($index + 1))) ?><!--"-->
+<!--                            --><?php //= $index > 0 ? 'loading="lazy"' : '' ?><!-- decoding="async">-->
+<!--                    </div>-->
                     <div class="carousel-item <?= $index === 0 ? 'active' : '' ?>">
+                        <?php if ($event->getDate() === date('Y-m-d')) : ?>
+                            <span class="badge-status status-live">En cours</span>
+                        <?php elseif ($event->getDate() < date('Y-m-d')) : ?>
+                            <span class="badge-status status-done">Terminé</span>
+                        <?php else : ?>
+                            <span class="badge-status status-incoming">À venir</span>
+                        <?php endif; ?>
+
                         <img src="<?= htmlspecialchars($image['src']) ?>" class="carousel-image"
-                            alt="<?= htmlspecialchars($image['alt'] ?? ($carouselLabel . ' - Image ' . ($index + 1))) ?>"
-                            <?= $index > 0 ? 'loading="lazy"' : '' ?> decoding="async">
+                             alt="<?= htmlspecialchars($image['alt'] ?? ($carouselLabel . ' - Image ' . ($index + 1))) ?>"
+                                <?= $index > 0 ? 'loading="lazy"' : '' ?> decoding="async">
                     </div>
                 <?php endforeach ?>
             </div>
+
+
 
             <button class="carousel-control next" onclick="moveSlide(1, '<?= $carouselId ?>')" aria-label="Suivant">
                 <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
